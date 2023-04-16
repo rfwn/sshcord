b6c00c870dd67187cd97bdc68bc76d9fd8093fe3
rfwn࿐ <m.erfan.majidi@Gmail.com>
2023-04-16 18:41:18 +0330

    so am i

--- src/config.ts
+++ src/config.ts
@@ -2,12 +2,9 @@
 require('dotenv').config();
 
 export default {
-    token: process.env.TOKEN,
-    clientId: process.env.CLIENT_ID,
-    ownerId: process.env.OWNER_ID,
-    serverId: process.env.SERVER_ID,
-    memesWhId: process.env.MEMES_WEBHOOK_ID,
-    memesWhToken: process.env.MEMES_WEBHOOK_TOKEN,
-    mongoUrl: process.env.MONGO_URL,
+    token: process.env.token,
+    clientId: process.env.client_id,
+    ownerId: process.env.owner_id,
+    serverId: process.env.server_id,
     debug: true,
 };
